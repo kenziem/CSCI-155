0a8083fa735a3aa95c11212e631cd2be31ff7d22
kenziem <kenzielovespink96@gmail.com>
2016-03-31 15:25:18 -0400

    first commit

---
+++ switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main ()
+{
+   // local variable declaration:
+   char grade = 'C';
+
+   switch(grade)
+   {
+   case 'A' :
+      cout << "Excellent!" << endl;
+      break;
+   case 'B' :
+   case 'C' :
+      cout << "Well done" << endl;
+      break;
+   case 'D' :
+      cout << "You passed" << endl;
+      break;
+   case 'F' :
+      cout << "Better try again" << endl;
+      break;
+   default :
+      cout << "Invalid grade" << endl;
+   }
+   cout << "Your grade is " << grade << endl;
+ 
+   return 0;
+}
