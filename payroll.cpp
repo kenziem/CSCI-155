2b877cb38c85000cfb72244fb0d3ffe700d0a8d6
kenziem <kenzielovespink96@gmail.com>
2016-04-28 14:25:28 -0400

    first upgrade

--- payroll.cpp
+++ payroll.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main() { 
 //setting up variables
-double hours, rate, pay; 
+double hours, rate, pay, weekly, A;
+  A = 7;
 
 // Get the number of hours worked. 
-cout << "How many hours did you work? "; 
+cout << "How many hours do you work in an average day? "; 
 cin >> hours; 
 
 // Get the hourly pay rate. 
@@ -15,9 +16,12 @@ cout << "How much do you get paid per hour? ";
 cin >> rate; 
 
 // Calculate the pay. 
-pay = hours * rate; 
+pay = hours * rate;
+weekly = pay * A;
 
 // Display the pay. 
-cout << "You have earned $" << pay << endl; 
+cout << "You have earned $" << pay << " in an average day" << endl;
+  cout << "You have earned $" << weekly << " in a week" << endl;
+  cout << "Good job!" << endl;
 return 0; 
 }
